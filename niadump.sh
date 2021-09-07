05e76ef23645eb0121cf175409bde98ed5335a82
isnipe <thalles.isnipe@gmail.com>
2021-09-07 15:44:18 -0300

    Added niadump.sh

--- niadump.sh
+++ niadump.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
-# Automação para analise PASSIVA da comunicação android
+# Automation for PASSIVE analysis of android communication
+# Need tcpdump binary in "/data/local/tmp/tcpdump"
 #
 
 PURPLE="\033[01;35m"
@@ -8,27 +9,27 @@ GRAY="\033[1;37m"
 NC="\033[0m"
 
 if test "$1" = "-h" ;then
-	echo "Uso de niadump"
-	echo -e "-h \t Mostra essa mensagem de ajuda."
-	echo -e "-p \t Extrai e apaga arquivo /sdcard/out.pcap do android"
+	echo "Usage of niadump"
+	echo -e "-h \t Show this help message."
+	echo -e "-p \t Extract and delete /sdcard/out.pcap file from android."
 	exit
 fi
 if test "$1" = "-p" ; then
-	echo -e "${GRAY}[*]${PURPLE} Transferindo${GRAY} /sdcard/out.pcap${NC}"
+	echo -e "${GRAY}[*]${PURPLE} Transferring${GRAY} /sdcard/out.pcap${NC}"
 	adb pull /sdcard/out.pcap
 
-	echo -e "${GRAY}[*]${PURPLE} Apagando${GRAY} /sdcard/out.pcap${NC}"
+	echo -e "${GRAY}[*]${PURPLE} Erasing${GRAY} /sdcard/out.pcap${NC}"
 	adb shell su -c rm /sdcard/out.pcap
 
-	echo -e "\n${GRAY}[*]${PURPLE} Abrindo wireshark${NC}"
+	echo -e "\n${GRAY}[*]${PURPLE} Opening WireShark${NC}"
 	nohup wireshark -r out.pcap > /dev/null 2>&1&
 	exit 1
 fi
 
-echo -e "${GRAY}[*]${PURPLE} Apagando${GRAY} /sdcard/out.pcap"
+echo -e "${GRAY}[*]${PURPLE} Erasing${GRAY} /sdcard/out.pcap"
 adb shell su -c rm /sdcard/out.pcap
 
-echo -e "\n${GRAY}[*]${PURPLE} Iniciando tcpdump ${NC}"
+echo -e "\n${GRAY}[*]${PURPLE} Starting tcpdump${NC}"
 adb shell su -c /data/local/tmp/tcpdump -i any -p -v -s 0 -w /sdcard/out.pcap
 
 
