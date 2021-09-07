63c82ce89a1c3b9f5bb81663383a9674d603eab2
isnipe <thalles.isnipe@gmail.com>
2021-09-07 15:50:16 -0300

    Added nipak.sh

--- nipak.sh
+++ nipak.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
+#############################################################
+# Extract apks from Apps downloaded from Google Play Store
+#
+# Use: .\nipak.sh com.instagram.android
+#############################################################
 path=$(adb shell su -c pm path $1 | grep base.apk | cut -d ":" -f 2)
 adb pull $path
 mv base.apk $1.apk
