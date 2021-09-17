8ca11cdf2a7240e4d7ff4119da0a57f9e709954d
isnipe <thalles.isnipe@gmail.com>
2021-09-17 01:30:46 -0300

    Remove the su of nipak

--- scripts/nipak.sh
+++ scripts/nipak.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #############################################################
-# Extract apks from Apps downloaded from Google Play Store
+# Extract APK file from Apps installed in the device.       
 #
 # Use: .\nipak.sh com.instagram.android
 #############################################################
-path=$(adb shell su -c pm path $1 | grep base.apk | cut -d ":" -f 2)
+
+path=$(adb shell pm path $1 | grep base.apk | cut -d ":" -f 2)
 adb pull $path
 mv base.apk $1.apk
