8814124f9bc05ecf33600ae770b99e1c96a96d2f
Nipe <77520996+i5nipe@users.noreply.github.com>
2022-05-09 22:18:35 -0300

    Create nisig.sh

---
+++ scripts/nisig.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Sign APK files
+
+if [ "$1" == "" ]
+then
+        echo "Usage: $0 aplication.apk"
+else
+        echo "Name of the file:"
+        read name
+
+        echo "Password:"
+        read pass
+
+        echo "Generating signature file..."
+        keytool -genkey -keystore $name.jks -storepass $pass -storetype jks -alias $name -keyalg rsa -dname "CN=NIPE" -
+        keypass $pass
+
+        echo "Signing apk"
+        jarsigner -keystore $name.jks -storepass $pass -storetype jks -sigalg sha1withrsa -digestalg sha1 $1 $name
+
+        echo "Verifying signature"
+        jarsigner -verify -certs -verbose $1
+fi
