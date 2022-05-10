75cc37e97b3435afc8ed1c8d9713bb348f566aa0
Nipe <77520996+i5nipe@users.noreply.github.com>
2022-05-09 21:48:57 -0300

    Added help option

--- scripts/nipak.sh
+++ scripts/nipak.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-#############################################################
 # Extract APK file from Apps installed in the device.       
-#
-# Use: .\nipak.sh com.instagram.android
-#############################################################
 
-path=$(adb shell pm path $1 | grep base.apk | cut -d ":" -f 2)
-adb pull $path
+if [ "$#" -eq 0 ] || [ "$1" = "-h" ]; then
+
+        echo -e "${PURPLE}Nipak${NC} -${GRAY}Extract APK${NC} from Apps installed in the device"
+        echo "Usage: nipak [ package ]"
+        echo "Ex: nipak com.instagram.android"
+        exit
+fi
+
+caminho=$(adb shell pm path $1 | grep base.apk | cut -d ":" -f 2)
+adb pull $caminho
 mv base.apk $1.apk
