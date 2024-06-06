e7f04a2486c03f00d9df5ca281aca7340bd7b435
i5nipe <77520996+i5nipe@users.noreply.github.com>
2024-06-06 01:51:53 -0300

    Update nisig.sh

--- scripts/nisig.sh
+++ scripts/nisig.sh
@@ -5,11 +5,12 @@ if [ "$1" == "" ]
 then
         echo "Usage: $0 aplication.apk"
 else
-        echo "Name of the file:"
-        read name
+        #echo "Name of the file:"
+        #read name
+        name="sig_key"
 
-        echo "Password(min: 6 caracters):"
-        read pass
+        #echo "Password(min: 6 caracters):"
+        pass="Default123"
 
         echo "Generating signature file..."
         keytool -genkey -keystore $name.jks -storepass $pass -storetype jks -alias $name -keyalg rsa -dname "CN=NIPE" -keypass $pass
